95d4d1aee0f7c179ce2d9c6075093fc2f59da581
Stepan Ivanov <stepan-ivanov-2005@bk.ru>
2025-05-02 15:16:00 +0300

    add initial commit

---
+++ guess_num.py
@@ -0,0 +1,14 @@
+from random import randint
+
+
+pc_num = randint(1, 100)
+print('Угадай число от 1 до 100')
+while True:
+    user_num = int(input('Введите число: '))
+    if user_num > pc_num:
+        print('Ваше число больше загаданного. Попробуйте снова.')
+    if user_num < pc_num:
+        print('Ваше число меньше загаданного. Попробуйте снова.')
+    if user_num == pc_num:
+        break
+print('Победа. Вы угадали загаданное число.')
